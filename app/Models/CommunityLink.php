165f021db0680db7aecae1b24817823f7423200f
Bruno Bernard <contact.brunobernard@gmail.com>
2026-02-02 19:54:01 +0400

    feat: added community links

--- app/Models/CommunityLink.php
+++ app/Models/CommunityLink.php
@@ -16,10 +16,14 @@ class CommunityLink extends Model
      * @var array<int, array{icon_key: string, name: string, description: string, url: string}>
      */
     protected array $rows = [
-        ['icon_key' => 'discord', 'name' => 'Discord', 'description' => 'Chat with the community', 'url' => '#'],
-        ['icon_key' => 'whatsapp', 'name' => 'WhatsApp', 'description' => 'Quick discussions', 'url' => '#'],
-        ['icon_key' => 'linkedin', 'name' => 'LinkedIn', 'description' => 'Follow updates', 'url' => '#'],
-        ['icon_key' => 'github-light', 'name' => 'GitHub', 'description' => 'Contribute to open source', 'url' => '#'],
+        ['icon_key' => 'youtube', 'name' => 'YouTube', 'description' => 'Watch meetup recordings', 'url' => 'https://www.youtube.com/@laravelphpmoris'],
+        ['icon_key' => 'twitter', 'name' => 'X (Twitter)', 'description' => 'Latest updates', 'url' => 'https://x.com/laravelphpmoris'],
+        ['icon_key' => 'bluesky', 'name' => 'Blue Sky', 'description' => 'Follow the community', 'url' => 'https://bsky.app/profile/laravelmoris.bsky.social'],
+        ['icon_key' => 'facebook', 'name' => 'Facebook', 'description' => 'Join the community', 'url' => 'https://www.facebook.com/laravelphpmoris'],
+        ['icon_key' => 'linkedin', 'name' => 'LinkedIn', 'description' => 'Follow updates', 'url' => 'https://www.linkedin.com/company/laravelphpmoris/'],
+        ['icon_key' => 'github-light', 'name' => 'GitHub', 'description' => 'Contribute to open source', 'url' => 'https://github.com/laravel-moris/'],
+        ['icon_key' => 'whatsapp', 'name' => 'WhatsApp', 'description' => 'Quick discussions', 'url' => 'https://chat.whatsapp.com/HqGA8K8QwoJ7ikhGQ5dE1a?mode=gi_t'],
+        ['icon_key' => 'discord', 'name' => 'Discord', 'description' => 'Chat with the community', 'url' => 'https://discord.gg/8vjXugjRfG'],
     ];
 
     protected array $schema = [
